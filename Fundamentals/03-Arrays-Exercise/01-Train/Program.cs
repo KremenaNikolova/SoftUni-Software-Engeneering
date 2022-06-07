5320eff61dc4ae766e92948ddb84d8df766d6f87
KremenaNikolova <aradia1992@gmail.com>
2022-06-07 14:27:51 +0300

    Kremena Nikolova

---
+++ Fundamentals/03-Arrays-Exercise/01-Train/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+            int[] numberOfPeoleOnEachWagon = new int[numberOfWagons];
+
+            int sumNumberOfPeople = 0;
+
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                numberOfPeoleOnEachWagon[i] = int.Parse(Console.ReadLine());
+
+                sumNumberOfPeople += numberOfPeoleOnEachWagon[i];
+            }
+            for (int j = 0; j < numberOfPeoleOnEachWagon.Length; j++)
+            {
+                Console.Write(numberOfPeoleOnEachWagon[j] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(sumNumberOfPeople);
+        }
+    }
+}
---
+++ Fundamentals/03-Arrays-Exercise/02-Common-Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02_Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                for (int j = 0; j < secondArray.Length; j++)
+                {
+                    if (firstArray[i] == secondArray[j])
+                    {
+                        Console.WriteLine(firstArray);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
