56d0ca16df8a63c0b9a2284d998a29fd5dcc98f3
KremenaNikolova <aradia1992@gmail.com>
2022-06-07 10:25:31 +0300

    Kremena Nikolova

--- Fundamentals/03-Arrays-Lab/08-Condense-Array-to-Number/Program.cs
+++ Fundamentals/03-Arrays-Lab/08-Condense-Array-to-Number/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08_Condense_Array_to_Number
 {
@@ -6,7 +7,26 @@ namespace _08_Condense_Array_to_Number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] randomDigits = Console.ReadLine().Split().Select(int.Parse).ToArray(); //10 15 20 25
+            int[] sumOfEveryTwoDigits = new int[randomDigits.Length-1];   
+
+            while (randomDigits.Length>1)
+            {
+                for (int i = 0; i < randomDigits.Length - 1; i++)
+                {
+                    sumOfEveryTwoDigits[i] = randomDigits[i] + randomDigits[i + 1]; //10+15 15+20 20+25
+
+                    if (i == randomDigits.Length-2)
+                    {
+                        randomDigits= sumOfEveryTwoDigits;
+                        sumOfEveryTwoDigits = new int[randomDigits.Length - 1];
+                    }
+                }
+                
+            }
+
+            Console.WriteLine(randomDigits[0]);
+            
         }
     }
 }
