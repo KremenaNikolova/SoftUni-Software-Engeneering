5a97862bae1c05bc2188b51dca247e89279ae386
KremenaNikolova <aradia1992@gmail.com>
2022-07-08 13:52:28 +0300

    Kremena Nikolova

--- Fundamentals/06-Objects-and-Classes-Exercise/06-Vehicle-Catalogue/Program.cs
+++ Fundamentals/06-Objects-and-Classes-Exercise/06-Vehicle-Catalogue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _06_Vehicle_Catalogue
 {
@@ -6,7 +7,69 @@ namespace _06_Vehicle_Catalogue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Vehicle> vehicle = new List<Vehicle>();
+            //List<Trucks> trucks = new List<Trucks>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string type = input[0];
+
+                if (type == "End")
+                {
+                    break;
+                }
+                string model = input[1];
+                string color= input[2];
+                int horsePower = int.Parse(input[3]);
+
+                
+                    var car = new Vehicle();
+                    car.Type = type;
+                    car.Model = model;
+                    car.Color = color;
+                    car.HorsePower = horsePower;
+                    vehicle.Add(car);
+                
+                   
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Close the Catalogue")
+                {
+                    return;
+                }
+                else 
+                {
+                    foreach (var item in collection)
+                    {
+
+                    }
+                }
+            }
+
+
         }
+
+        class Vehicle
+        {
+
+            public string Type { get; set; }
+            public string Model { get; set; }
+            public string Color { get; set; }
+            public int HorsePower { get; set; }
+        }
+
+        //class Trucks
+        //{
+        //    public string Type { get; set; }
+        //    public string Model { get; set; }
+        //    public string Color { get; set; }
+        //    public int HorsePower { get; set; }
+        //}
     }
 }
