0b6ece86282d882ca0beb9eb5d0ccd7dd41f17ea
KremenaNikolova <aradia1992@gmail.com>
2022-07-13 15:50:34 +0300

    Kremena Nikolova

--- Fundamentals/07-Associative-Arrays-Exercise/03-Orders/Program.cs
+++ Fundamentals/07-Associative-Arrays-Exercise/03-Orders/Program.cs
@@ -7,9 +7,47 @@ namespace _03_Orders
     {
         static void Main(string[] args)
         {
-            Dictionary<string, double> productsInformation = new Dictionary<string, double>();
+            Dictionary<string, int> productsQuantity = new Dictionary<string, int>();
+            Dictionary<string, decimal> productsPrice = new Dictionary<string, decimal>();
 
-            string[] input=Console.ReadLine().Split();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string nameOfProduct = input[0];
+                if (nameOfProduct=="buy")
+                {
+                    break;
+                }
+
+                decimal price = decimal.Parse(input[1]);
+                int quantity = int.Parse(input[2]);
+
+                if (!productsQuantity.ContainsKey(nameOfProduct))
+                {
+                    productsQuantity.Add(nameOfProduct, quantity);
+                    productsPrice.Add(nameOfProduct, price);
+                }
+                else
+                {
+                    productsQuantity[nameOfProduct] += quantity;
+                    productsPrice[nameOfProduct] = price;
+                }
+                
+            }
+
+            foreach (var quantity in productsQuantity)
+            {
+                foreach (var price in productsPrice)
+                {
+                    if (quantity.Key == price.Key)
+                    {
+                        Console.WriteLine($"{quantity.Key} -> {quantity.Value * price.Value}");
+                    }
+                }
+            }
+
+
         }
     }
 }
---
+++ Fundamentals/07-Associative-Arrays-Exercise/04-SoftUni-Parking/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> registeredUsers = new Dictionary<string, string>();
+
+            int numOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string command = input[0];
+                string username = input[1];
+                string licensePlateNumber=string.Empty;
+                if (input.Length>2)
+                {
+                    licensePlateNumber = input[2];
+                }
+
+                if (command == "register")
+                {
+                    if (!registeredUsers.ContainsKey(username))
+                    {
+                        registeredUsers.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (!registeredUsers.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        registeredUsers.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+
+            foreach (var user in registeredUsers)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+    }
+}
