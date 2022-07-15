1e23443716ff0b95afa73732f6f4f3bc06be2f79
KremenaNikolova <aradia1992@gmail.com>
2022-07-15 13:58:45 +0300

    Kremena Nikolova

---
+++ Fundamentals/07-Associative-Arrays-Exercise/05-Courses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05_Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> studentInfo = new Dictionary<string, List<string>>();
+            //List<string> students = new List<string>();
+
+            string[] information = Console.ReadLine().Split(" : ");
+            
+
+            while (information[0] != "end")
+            {
+                string courseName = information[0];
+                string studentName = information[1];
+                if (!studentInfo.ContainsKey(courseName))
+                {
+                    studentInfo.Add(courseName, new List<string>());
+                    studentInfo[courseName].Add(studentName);
+                }
+                else
+                {
+                    studentInfo[courseName].Add(studentName);
+                }
+                information = Console.ReadLine().Split(" : ");
+
+            }
+
+            foreach (var course in studentInfo)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var students in studentInfo)
+                {
+                    if (course.Key==students.Key)
+                    {
+                        for (int i = 0; i < course.Value.Count; i++)
+                        {
+                            Console.WriteLine($"-- {students.Value[i]}");
+                        }
+                        //Console.WriteLine($"-- {students.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
