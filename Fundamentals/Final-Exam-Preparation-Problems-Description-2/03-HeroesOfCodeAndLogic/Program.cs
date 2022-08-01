d55e64e2223388ba6bbcf08cb1d72d6d2d83521e
KremenaNikolova <aradia1992@gmail.com>
2022-08-01 20:36:12 +0300

    Kremena Nikolova

---
+++ Fundamentals/Final-Exam-Preparation-Problems-Description-2/03-HeroesOfCodeAndLogic/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_Pirates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfHeroes = int.Parse(Console.ReadLine());
+            Dictionary<string, List<int>> heroInformation = new Dictionary<string, List<int>>();
+
+            for (int i = 0; i < numberOfHeroes; i++)
+            {
+                string[] heroInfo = Console.ReadLine().Split();
+                string heroName = heroInfo[0];
+                int heroHp = int.Parse(heroInfo[1]);
+                int heroMp = int.Parse(heroInfo[2]);
+
+                
+                heroInformation.Add(heroName, new List<int> { heroHp, heroMp });
+
+                //-	a hero can have a maximum of 100 HP and 200 MP
+            }
+
+            string[] command = Console.ReadLine().Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "End")
+            {
+                string action = command[0];
+                string heroName = command[1];
+
+                switch (action)
+                {
+                    case "CastSpell":
+                        CastSpell(heroName, int.Parse(command[2]), command[3], heroInformation);
+                        break;
+                    case "TakeDamage":
+                        TakeDamage(heroName, int.Parse(command[2]), command[3], heroInformation);
+                        break;
+                    case "Recharge":
+                        Recharge(heroName, int.Parse(command[2]), heroInformation);
+                        break;
+                    case "Heal":
+                        Heal(heroName, int.Parse(command[2]), heroInformation);
+                        break;
+                }
+
+                command = Console.ReadLine().Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var hero in heroInformation)
+            {
+                Console.WriteLine(hero.Key);
+                Console.WriteLine($"  HP: {hero.Value[0]}");
+                Console.WriteLine($"  MP: {hero.Value[1]}");
+            }
+        }
+
+        private static void CastSpell(string heroName, int mpNeeded, string spellName, Dictionary<string, List<int>> heroInformation)
+        {
+            if (heroInformation[heroName][1]>=mpNeeded)
+            {
+                heroInformation[heroName][1] -= mpNeeded;
+                Console.WriteLine($"{heroName} has successfully cast {spellName} and now has {heroInformation[heroName][1]} MP!");
+                return;
+            }
+            Console.WriteLine($"{heroName} does not have enough MP to cast {spellName}!");
+        }
+
+        private static void TakeDamage(string heroName, int damage, string attacker, Dictionary<string, List<int>> heroInformation)
+        {
+            heroInformation[heroName][0] -= damage;
+            if (heroInformation[heroName][0]>0)
+            {
+                Console.WriteLine($"{heroName} was hit for {damage} HP by {attacker} and now has {heroInformation[heroName][0]} HP left!");
+                return;
+            }
+            Console.WriteLine($"{heroName} has been killed by {attacker}!");
+            heroInformation.Remove(heroName);
+        }
+
+        private static void Recharge(string heroName, int amount, Dictionary<string, List<int>> heroInformation)
+        {
+            heroInformation[heroName][1] += amount;
+            if (heroInformation[heroName][1]>200)
+            {
+                heroInformation[heroName][1] = 200;
+            }
+            Console.WriteLine($"{heroName} recharged for {amount} MP!");
+        }
+
+        private static void Heal(string heroName, int amount, Dictionary<string, List<int>> heroInformation)
+        {
+            heroInformation[heroName][0] += amount;
+            if (heroInformation[heroName][0] > 100)
+            {
+                heroInformation[heroName][0] = 100;
+            }
+            Console.WriteLine($"{heroName} healed for {amount} HP!");
+        }
+    }
+}
--- Fundamentals/Final-Exam-Preparation-Problems-Description-2/03-Pirates/Program.cs
+++ Fundamentals/Final-Exam-Preparation-Problems-Description-2/03-Pirates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03_Pirates
 {
@@ -6,7 +7,42 @@ namespace _03_Pirates
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfHeroes = int.Parse(Console.ReadLine());
+            Dictionary<string, int> heroAndHP = new Dictionary<string, int>();
+            Dictionary<string, int> heroAndMP = new Dictionary<string, int>();
+
+            for (int i = 0; i < numberOfHeroes; i++)
+            {
+                string[] heroInfo = Console.ReadLine().Split();
+                string heroName = heroInfo[0];
+                int heroHp = int.Parse(heroInfo[1]);
+                int heroMp = int.Parse(heroInfo[2]);
+
+                heroAndHP.Add(heroName, heroHp);
+                heroAndMP.Add(heroName, heroMp);
+
+                //-	a hero can have a maximum of 100 HP and 200 MP
+            }
+
+            string[] command = Console.ReadLine().Split(" – ");
+
+            while (command[0]!="End")
+            {
+                string action = command[0];
+                string heroName = command[1];
+                if (action== "CastSpell")
+                {
+                    int mpNeed = int.Parse(command[2]);
+                    string spellName = command[3];
+
+                    if (mpNeed==heroAndHP.)
+                    {
+
+                    }
+                }
+
+                command = Console.ReadLine().Split(" – ");
+            }
         }
     }
 }
