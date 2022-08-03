8cc73f0e2efb0364085186f5a8b731a9958a221a
KremenaNikolova <aradia1992@gmail.com>
2022-08-03 13:44:55 +0300

    Kremena Nikolova

--- Fundamentals/Fundamentals-Final-Exam-Exercising/07-World-Tour/Program.cs
+++ Fundamentals/Fundamentals-Final-Exam-Exercising/07-World-Tour/Program.cs
@@ -6,7 +6,58 @@ namespace _07_World_Tour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string travel = Console.ReadLine();
+
+            string[] command = Console.ReadLine().Split(":");
+
+            while (command[0] != "Travel")
+            {
+                string action = command[0];
+                switch (action)
+                {
+                    case "Add Stop":
+                        travel = AddStop(int.Parse(command[1]), command[2], travel);
+                        break;
+                    case "Remove Stop":
+                        travel = RemoveStop(int.Parse(command[1]), int.Parse(command[2]), travel);
+                        break;
+                    case "Switch":
+                        travel = Switch(command[1], command[2], travel);
+                        break;
+                }
+                Console.WriteLine(travel);
+                command = Console.ReadLine().Split(":");
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {travel}");
+        }
+
+        static string AddStop(int index, string str, string travel)
+        {
+            if (travel.Length > index)
+            {
+                travel = travel.Insert(index, str);
+            }
+            return travel;
+        }
+
+        static string RemoveStop(int startIndex, int endIndex, string travel)
+        {
+            if (travel.Length > endIndex && startIndex > -1)
+            {
+                travel = travel.Remove(startIndex, endIndex-startIndex+1);
+            }
+
+            return travel;
+        }
+
+        static string Switch(string oldString, string newString, string travel)
+        {
+            if (travel.Contains(oldString))
+            {
+                travel = travel.Replace(oldString, newString);
+            }
+            return travel;
         }
     }
 }
