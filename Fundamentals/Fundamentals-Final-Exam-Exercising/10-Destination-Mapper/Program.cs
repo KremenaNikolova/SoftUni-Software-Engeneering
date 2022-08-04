d64b180acde3654a25cef63baf37a9ca56c9765f
KremenaNikolova <aradia1992@gmail.com>
2022-08-04 14:29:08 +0300

    Kremena Nikolova

---
+++ Fundamentals/Fundamentals-Final-Exam-Exercising/10-Destination-Mapper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _10_Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string locationsInput = Console.ReadLine();
+            string pattern = @"(=|/{1})([A-Z][A-Za-z]{2,})\1";
+            List<string> locations = new List<string>();
+            
+            MatchCollection matches = Regex.Matches(locationsInput, pattern);
+            int counter = 0;
+
+            foreach (var match in matches)
+            {
+                string location = match.ToString();
+                string validLocation = string.Empty;
+                for (int i = 1; i < location.Length-1; i++)
+                {
+                    counter++;
+                    validLocation += location[i];
+                }
+                locations.Add(validLocation);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", locations)}");
+            Console.WriteLine($"Travel Points: {counter}");
+        }
+    }
+}
---
+++ Fundamentals/Fundamentals-Final-Exam-Exercising/11-Plant-Discovery/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_Plant_Discovery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+
+            SortedDictionary<string, int> plants = new SortedDictionary<string, int>();
+            SortedDictionary<string, List<double>> plantsRating = new SortedDictionary<string, List<double>>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                string[] plantInformation = Console.ReadLine().Split("<->");
+                string key = plantInformation[0];
+                int value = int.Parse(plantInformation[1]);
+                if (!plants.ContainsKey(key))
+                {
+                    plants.Add(key, value);
+                    plantsRating.Add(key, new List<double>{0, 0 });
+                }
+                else
+                {
+                    plants[key] = value;
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(new char[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "Exhibition")
+            {
+                string action = command[0];
+                if (!plants.ContainsKey(command[1]))
+                {
+                    Console.WriteLine("error");
+                    command = Console.ReadLine().Split(new char[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+                switch (action)
+                {
+                    case "Rate:":
+                        Rate(command[1], double.Parse(command[2]), plants, plantsRating);
+                        break;
+                    case "Update:":
+                        Update(command[1], int.Parse(command[2]), plants, plantsRating);
+                        break;
+                    case "Reset:":
+                        Reset(command[1], plants, plantsRating);
+                        break;
+                }
+
+                command = Console.ReadLine().Split(new char[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            Console.WriteLine("Plants for the exhibition:");
+
+            var sorted = plantsRating
+                .OrderByDescending(x => x.Value[0])
+                .ThenByDescending(x => plantsRating[x.Key].Count > 0
+                    ? plantsRating[x.Key].Sum() / plantsRating[x.Key].Count
+                    : 0.0);
+
+            foreach (var rating in sorted)
+            {
+                foreach (var plant in plants)
+                {
+                    if (rating.Key==plant.Key)
+                    {
+                        Console.WriteLine($"- {plant.Key}; Rarity: {plant.Value}; Rating: {rating.Value}");
+                    }
+                }
+            }
+        }
+
+        private static void Rate(string plant, double rating, SortedDictionary<string, int> plants, SortedDictionary<string, List<double>> plantsRating)
+        {
+
+            plantsRating[plant][0] += rating;
+            plantsRating[plant][1]++;
+        }
+
+        private static void Update(string plant, int rarity, SortedDictionary<string, int> plants, SortedDictionary<string, List<double>> plantsRating)
+        {
+            plants[plant] = rarity;
+        }
+
+        private static void Reset(string plant, SortedDictionary<string, int> plants, SortedDictionary<string, List<double>> plantsRating)
+        {
+            plantsRating[plant][0] = 0;
+        }
+    }
+}
