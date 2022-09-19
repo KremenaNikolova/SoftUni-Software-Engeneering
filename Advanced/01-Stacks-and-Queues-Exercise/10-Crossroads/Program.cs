63c82058bd7fab8b4a7b9e3ca1382d5eeb6df864
KremenaNikolova <aradia1992@gmail.com>
2022-09-19 21:32:39 +0300

    Kremena Nikolova

--- Advanced/01-Stacks-and-Queues-Exercise/10-Crossroads/Program.cs
+++ Advanced/01-Stacks-and-Queues-Exercise/10-Crossroads/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _10_Crossroads
 {
@@ -6,7 +7,67 @@ namespace _10_Crossroads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int greenDuartion = int.Parse(Console.ReadLine());
+            int freeWindowDuration = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            Queue<char> car = new Queue<char>();
+            int carsPassed = 0;
+
+            string command = Console.ReadLine();
+            while (command!="END")
+            {
+                if (command!="green")
+                {
+                    string carType = command;
+                    queue.Enqueue(carType);
+                }
+                else if(queue.Count > 0)
+                {
+                    string currCarr = queue.Dequeue();
+                    char[] carr = currCarr.ToCharArray();
+                    car = new Queue<char>(carr);
+                    for (int duration = 0; duration < greenDuartion; duration++)
+                    {
+                        car.Dequeue();
+                        if (car.Count == 0)
+                        {
+                            carsPassed++;
+                            if (queue.Count>0 && duration + 1 != greenDuartion)
+                            {
+                                currCarr = queue.Dequeue();
+                                carr = currCarr.ToCharArray();
+                                car = new Queue<char>(carr);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (car.Count>0)
+                    {
+                        for (int free = 0; free < freeWindowDuration; free++)
+                        {
+                            car.Dequeue();
+                            if (car.Count==0)
+                            {
+                                carsPassed++;
+                                break;
+                            }
+                        }
+                        if (car.Count>0)
+                        {
+                            Console.WriteLine("A crash happened!");
+                            Console.WriteLine($"{currCarr} was hit at {car.Peek()}.");
+                            return;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{carsPassed} total cars passed the crossroads.");
         }
     }
 }
---
+++ Advanced/01-Stacks-and-Queues-Exercise/11-Key-Revolver/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11_Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int eachBulletPrice = int.Parse(Console.ReadLine());
+            int sizeGunBarrel = int.Parse(Console.ReadLine());
+            int[] bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bulletsStack = new Stack<int>(bullets);
+            int[] locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> locksQueue = new Queue<int>(locks);
+            int allMoney = int.Parse(Console.ReadLine());
+
+            int bulletsCounter = 0;
+            while (locksQueue.Count > 0)
+            {
+                bulletsCounter++;
+                int currLock = locksQueue.Peek();
+                if (currLock >= bulletsStack.Pop())
+                {
+                    Console.WriteLine("Bang!");
+                    locksQueue.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+                if (bulletsCounter % sizeGunBarrel == 0)
+                {
+                    if (bulletsStack.Count == 0)
+                    {
+                        Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+                        return;
+                    }
+                    else if (locksQueue.Count == 0)
+                    {
+                        Console.WriteLine("Reloading!");
+                        Console.WriteLine($"{bulletsStack.Count} bullets left. Earned ${allMoney -= bulletsCounter * eachBulletPrice}");
+                        return;
+                    }
+                    Console.WriteLine("Reloading!");
+
+                }
+            }
+            Console.WriteLine($"{bulletsCounter} bullets left. Earned ${allMoney}");
+
+        }
+    }
+}
