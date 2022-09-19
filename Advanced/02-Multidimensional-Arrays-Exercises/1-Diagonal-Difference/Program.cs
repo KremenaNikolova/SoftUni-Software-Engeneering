4f1ece3cd4c43cf45ebb5a443ab4464478fab564
KremenaNikolova <aradia1992@gmail.com>
2022-09-20 02:01:23 +0300

    Kremena Nikolova

---
+++ Advanced/02-Multidimensional-Arrays-Exercises/1-Diagonal-Difference/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int sumLeftToRightDiagonal = 0;
+            int sumRightToLeftDiagonal = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (col==row)
+                    {
+                        sumLeftToRightDiagonal += matrix[row, col];
+                    }
+                }
+            }
+
+            for (int row = matrix.GetLength(0)-1; row >= 0; row--)
+            {
+                int col = matrix.GetLength(1) - 1;
+                sumRightToLeftDiagonal += matrix[row, col - row];
+            }
+
+            Console.WriteLine(Math.Abs(sumLeftToRightDiagonal-sumRightToLeftDiagonal));
+        }
+    }
+}
---
+++ Advanced/02-Multidimensional-Arrays-Exercises/2-Squares-in-Matrix/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = new char[dimensions[0], dimensions[1]];
+            int counter = 0;
+
+            if (matrix.GetLength(0)<1 || matrix.GetLength(1)<1)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (col+1<matrix.GetLength(1) && row+1<matrix.GetLength(0))
+                    {
+                        if (isEqualChars(row, col, matrix))
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+
+        }
+
+        static bool isEqualChars(int row, int col, char[,] matrix)
+        {
+            return
+                matrix[row, col] == matrix[row, col + 1]
+                && matrix[row, col] == matrix[row + 1, col]
+                && matrix[row, col] == matrix[row + 1, col + 1];
+        }
+    }
+}
+
