c2948b4f25e715c49ac07ba9167fd0e1d4a306d7
KremenaNikolova <aradia1992@gmail.com>
2022-09-27 20:05:28 +0300

    Kremena Nikolova

--- Advanced/04-Streams-Files-and-Directories-Lab/05-Extract-Special-Bytes/Program.cs
+++ Advanced/04-Streams-Files-and-Directories-Lab/05-Extract-Special-Bytes/Program.cs
@@ -1,8 +1,11 @@
 ﻿//You are given a binary file (e. g. example.png) and a text file (e. g. bytes.txt), holding a list of bytes in the range [0…255]. Write a program to extract occurrences of all given bytes from the input file to an output binary file (e. g. output.bin).
 
-namespace ExtractSpecialBytes
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExtractBytes
 {
-    public class ExtractSpecialBytes
+    public class ExtractBytes
     {
         static void Main()
         {
@@ -15,7 +18,32 @@ namespace ExtractSpecialBytes
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
-            // TODO: write your code here…
+           using(FileStream bytes = new FileStream(bytesFilePath, FileMode.Open))
+            {
+                using (FileStream binary = new FileStream(binaryFilePath, FileMode.Open))
+                {
+                    byte[] bytesBuffer = new byte[bytes.Length];
+                    bytes.Read(bytesBuffer, 0, (int)bytes.Length);
+
+                    byte[] binaryBuffer = new byte[binary.Length];
+                    binary.Read(binaryBuffer, 0, (int)binary.Length);
+
+                    using (FileStream output = new FileStream(outputPath, FileMode.Create))
+                    {
+                        for (int i = 0; i < binaryBuffer.Length; i++)
+                        {
+                            for (int j = 0; j < bytesBuffer.Length; j++)
+                            {
+                                if (binaryBuffer[i] == bytesBuffer[j])
+                                {
+                                    output.Write(new byte[] { binaryBuffer[i] });
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+            }
         }
     }
 }
---
+++ Advanced/04-Streams-Files-and-Directories-Lab/06-Split-Merge-Binary-Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06_Split_Merge_Binary_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
