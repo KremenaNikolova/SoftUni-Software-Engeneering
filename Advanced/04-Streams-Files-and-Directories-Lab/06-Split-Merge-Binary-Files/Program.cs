714082a44895f0977eb0b3a3fb4c0059253665d6
KremenaNikolova <aradia1992@gmail.com>
2022-09-27 23:02:21 +0300

    Kremena Nikolova

--- Advanced/04-Streams-Files-and-Directories-Lab/06-Split-Merge-Binary-Files/Program.cs
+++ Advanced/04-Streams-Files-and-Directories-Lab/06-Split-Merge-Binary-Files/Program.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿//You are given an input binary file (e. g. example.png). Write a program to split it into two equal-sized files (e. g. part-1.bin and part-2.bin). When the input file size is an odd number, the first part should be 1 byte bigger than the second.
+//After splitting the input file, join the obtained files into a new file(e.g.example - joined.png).The obtained result file should be the same as the initial input file.
 
-namespace _06_Split_Merge_Binary_Files
+
+using System.IO;
+
+namespace SplitMergeBinaryFile
 {
-    internal class Program
+    public class SplitMergeBinaryFile
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string sourceFilePath = @"..\..\..\Files\example.png";
+            string joinedFilePath = @"..\..\..\Files\example-joined.png";
+            string partOnePath = @"..\..\..\Files\part-1.bin";
+            string partTwoPath = @"..\..\..\Files\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        //Write a program to split it into two equal-sized files
+        //When the input file size is an odd number, the first part should be 1 byte bigger than the second.
+        //After splitting the input file, join the obtained files into a new file (e. g. example-joined.png). 
+        //The obtained result file should be the same as the initial input file.
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using (FileStream original = new FileStream(sourceFilePath, FileMode.Open))
+            {
+                using(FileStream partOne = new FileStream(partOneFilePath, FileMode.Create))
+                {
+                    int odd = original.Length % 2 == 1 ? 1 : 0;
+                    byte[] buffer = new byte[original.Length/2+odd];
+                    original.Read(buffer);
+                    partOne.Write(buffer);
+                }
+                using(FileStream partTwo = new FileStream(partTwoFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[original.Length / 2];
+                    original.Read(buffer);
+                    partTwo.Write(buffer);
+                }
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (FileStream merged = new FileStream(joinedFilePath, FileMode.Create))
+            {
+                using(FileStream partOne = new FileStream(partOneFilePath, FileMode.Open))
+                {
+                    byte[] buffer =new byte [partOne.Length];
+                    partOne.Read(buffer);
+                    merged.Write(buffer);
+                }
+                using(FileStream partTwo = new FileStream(partTwoFilePath, FileMode.Open))
+                {
+                    byte[] buffer = new byte[partTwo.Length];
+                    partTwo.Read(buffer);
+                    merged.Write(buffer);
+                }
+            }
         }
     }
 }
---
+++ Advanced/04-Streams-Files-and-Directories-Lab/07-Folder-Size/Program.cs
@@ -0,0 +1,35 @@
+﻿//You are given a folder in the file system (e. g. TestFolder). Calculate the size of all files in the folder and its subfolders. The result should be written to another text (e. g. оutput.txt) file in kilobytes.
+
+using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            DirectoryInfo folder = new DirectoryInfo(folderPath);
+            FileInfo[] files = folder.GetFiles("*", SearchOption.AllDirectories);
+            decimal sumBytes = 0;
+
+            foreach (FileInfo file in files)
+            {
+                sumBytes += file.Length;
+            }
+
+            using(StreamWriter output = new StreamWriter(outputFilePath))
+            {
+                output.Write(sumBytes/1024);
+            }
+        }
+    }
+}
+
