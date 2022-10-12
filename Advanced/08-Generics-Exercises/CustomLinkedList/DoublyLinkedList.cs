7ddda056408d606fe87e5952c54faf3a0f8c3d99
KremenaNikolova <aradia1992@gmail.com>
2022-10-12 18:45:28 +0300

    Kremena Nikolova

---
+++ Advanced/08-Generics-Exercises/CustomLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        private class ListNode
+        {
+            public T Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+        }
+        private ListNode head;
+        private ListNode tail;
+        public int Count { get; set; }
+
+        public void AddFirst(T value)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(value);
+            }
+            else
+            {
+                ListNode newHead = new ListNode(value);
+                newHead.NextNode = this.head;
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            if (this.Count == 0)
+            {
+                this.tail = this.head = new ListNode(value);
+            }
+            else
+            {
+                ListNode newTail = new ListNode(value);
+                newTail.PreviousNode = this.tail;
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            T firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+            this.Count--;
+            return firstElement;
+
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            T lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currNode = this.head;
+            while (currNode != null)
+            {
+                action(currNode.Value);
+                currNode = currNode.NextNode;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+            int counter = 0;
+            ListNode currNode = this.head;
+            while (currNode != null)
+            {
+                array[counter] = currNode.Value;
+                currNode = currNode.NextNode;
+                counter++;
+            }
+            return array;
+        }
+    }
+
+}
---
+++ Advanced/08-Generics-Exercises/CustomLinkedList/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList<string> list = new DoublyLinkedList<string>();
+
+            list.AddFirst("Kremena");
+            list.AddFirst("Valerieva");
+            list.AddLast("Nikolova");
+
+            list.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
