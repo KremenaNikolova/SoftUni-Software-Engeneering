0b9a776828ea9e29ea6c27b17564f01498479a02
KremenaNikolova <aradia1992@gmail.com>
2022-10-14 12:24:00 +0300

    Initial commit

---
+++ Advanced/09-Iterators-and-Comparators-Exercises/4Froggy/Lake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> numbers;
+
+        public Lake(List<int> numbers)
+        {
+            this.numbers = numbers;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < numbers.Count; i=i+2)
+            {
+                yield return numbers[i];
+            }
+            for (int i = numbers.Count-1; i >= 0; i--)
+            {
+                if (i%2!=0)
+                {
+                    yield return numbers[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Advanced/09-Iterators-and-Comparators-Exercises/4Froggy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            Lake jumps = new Lake(input);
+
+            Console.WriteLine(string.Join(", ",jumps));
+
+
+        }
+    }
+}
