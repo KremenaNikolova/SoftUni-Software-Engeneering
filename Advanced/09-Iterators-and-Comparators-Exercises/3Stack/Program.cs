2d66f712a7cd38d1eb6f4ba15260fb7402eb2cb8
KremenaNikolova <aradia1992@gmail.com>
2022-10-14 11:57:51 +0300

    Initial commit

--- Advanced/09-Iterators-and-Comparators-Exercises/3Stack/Program.cs
+++ Advanced/09-Iterators-and-Comparators-Exercises/3Stack/Program.cs
@@ -2,11 +2,45 @@
 
 namespace _3Stack
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+            Stack<int> stack = new Stack<int>();
+
+            while (input[0] !="END")
+            {
+                if (input[0]=="Push")
+                {
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        stack.Push(int.Parse(input[i]));
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (InvalidOperationException exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+
+                input = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ Advanced/09-Iterators-and-Comparators-Exercises/3Stack/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private T[] items;
+        private const int capacity = 4;
+
+        public Stack()
+        {
+            this.items = new T[capacity];
+        }
+        public int Count { get; private set; }
+
+        public void Push(T element)
+        {
+            if (items.Length==Count)
+            {
+                Resize();
+            }
+            items[Count] = element;
+            Count++;
+        }
+        public T Pop()
+        {
+           if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+           T removedElement = items[Count-1];
+            Count--;
+            return removedElement;
+        }
+
+        public void Resize()
+        {
+            T[] copyItems = new T[items.Length * 2];
+            for (int i = 0; i < Count; i++)
+            {
+                copyItems[i] = items[i];
+            }
+            items = copyItems;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count-1; i >= 0; i--)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
