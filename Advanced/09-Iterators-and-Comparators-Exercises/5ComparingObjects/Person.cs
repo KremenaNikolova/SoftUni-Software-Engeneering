c6e941bd186099bd4111161933347f49cd426f0e
KremenaNikolova <aradia1992@gmail.com>
2022-10-15 14:43:32 +0300

    Initial commit

---
+++ Advanced/09-Iterators-and-Comparators-Exercises/5ComparingObjects/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _5ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person()
+        {
+            Name = this.name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            return 
+        }
+    }
+}
---
+++ Advanced/09-Iterators-and-Comparators-Exercises/5ComparingObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
