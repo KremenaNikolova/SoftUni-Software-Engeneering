b04b3f638d967d6aab479129d0b99b9310ad7ab0
KremenaNikolova <aradia1992@gmail.com>
2022-10-16 10:08:35 +0300

    Initial commit

---
+++ Advanced/09-Iterators-and-Comparators-Exercises/6EqualityLogic/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _6EqualityLogic
+{
+    internal class Person : IComparable<Person>
+    {
+        private int personID;
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int PersonID { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (Name.CompareTo(other.Name) != 0)
+            {
+                return Name.CompareTo(other.Name);
+            }
+            return Age.CompareTo(other.Age);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+            if (person == null) return false;
+
+            return  Name==person.Name && Age==person.Age;
+        }
+        public override int GetHashCode()
+        {
+            int hashCode = Name.GetHashCode() + Age.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ Advanced/09-Iterators-and-Comparators-Exercises/6EqualityLogic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6EqualityLogic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> hashSet = new HashSet<Person>();
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            int inputLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] personInformation = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personInformation[0];
+                int age = int.Parse(personInformation[1]);
+
+                Person person = new Person
+                {
+                    Name = name,
+                    Age = age,
+                };
+                hashSet.Add(person);
+                sortedSet.Add(person);
+            }
+
+            Console.WriteLine(hashSet.Count);
+            Console.WriteLine(sortedSet.Count);
+        }
+    }
+}
