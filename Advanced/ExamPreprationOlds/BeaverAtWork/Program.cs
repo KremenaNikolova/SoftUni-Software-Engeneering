f725de8acc8fd15428bde023957ec2a59ca977f2
KremenaNikolova <aradia1992@gmail.com>
2022-10-20 13:43:11 +0300

    Initial commit

---
+++ Advanced/ExamPreprationOlds/BeaverAtWork/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeaverAtWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char[,] pond = new char[size, size];
+            int rowPosition = 0;
+            int colPosition = 0;
+            int branchesCount = 0;
+            Stack<char> collectedBranch = new Stack<char>();
+            int collectedFish = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                char[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int col = 0; col < size; col++)
+                {
+                    pond[row,col]=input[col];
+                    if (pond[row,col]=='B')
+                    {
+                        rowPosition = row;
+                        colPosition = col;
+                    }
+                    if (char.IsLetter(pond[row,col]) && char.IsLower(pond[row,col]))
+                    {
+                        branchesCount++;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();    //- F e -
+                                                    //- B F y
+                                                    //- - - q
+                                                    //- - z x
+
+            while (command!="end")
+            {
+                switch (command)
+                {
+                    case "up":
+                        rowPosition--;
+                        if (!isValidPosition(rowPosition, colPosition, pond))
+                        {
+                            rowPosition++;
+                            if (collectedBranch.Count>0)
+                            {
+                                collectedBranch.Pop();
+                            }
+                        }
+                        else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                        {
+                            branchesCount--;
+                            collectedBranch.Push(pond[rowPosition,colPosition]);
+                            pond[rowPosition, colPosition] = '-';
+
+                        }
+                        else if (pond[rowPosition, colPosition] == 'F')
+                        {
+                            collectedFish++;
+                            pond[rowPosition, colPosition] = '-';
+                            rowPosition = 0;
+
+                            if (pond[rowPosition,colPosition]=='F')
+                            {
+                                collectedFish++;
+                                pond[rowPosition, colPosition] = '-';
+                                rowPosition = pond.GetLength(0)-1;
+                                if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                                {
+                                    branchesCount--;
+                                    collectedBranch.Push(pond[rowPosition,colPosition]);
+                                    pond[rowPosition, colPosition] = '-';
+                                }
+                                pond[rowPosition, colPosition] = '-';
+                            }
+                            else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                            {
+                                branchesCount--;
+                                collectedBranch.Push(pond[rowPosition, colPosition]);
+                                pond[rowPosition, colPosition] = '-';
+
+                            }
+                        }
+                        break;
+                    case "down":
+                        rowPosition++;
+                        if (!isValidPosition(rowPosition, colPosition, pond))
+                        {
+                            rowPosition--;
+                            if (collectedBranch.Count > 0)
+                            {
+                                collectedBranch.Pop();
+                            }
+                        }
+                        else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                        {
+                            branchesCount--;
+                            collectedBranch.Push(pond[rowPosition, colPosition]);
+                            pond[rowPosition, colPosition] = '-';
+
+                        }
+                        else if (pond[rowPosition, colPosition] == 'F')
+                        {
+                            collectedFish++;
+                            pond[rowPosition, colPosition] = '-';
+                            rowPosition = pond.GetLength(0) - 1;
+
+                            if (pond[rowPosition, colPosition] == 'F')
+                            {
+                                collectedFish++;
+                                pond[rowPosition, colPosition] = '-';
+                                rowPosition = 0;
+                                if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                                {
+                                    branchesCount--;
+                                    collectedBranch.Push(pond[rowPosition, colPosition]);
+                                    pond[rowPosition, colPosition] = '-';
+                                }
+                                pond[rowPosition, colPosition] = '-';
+                            }
+                            else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                            {
+                                branchesCount--;
+                                collectedBranch.Push(pond[rowPosition, colPosition]);
+                                pond[rowPosition, colPosition] = '-';
+
+                            }
+                        }
+                        break;
+                    case "left":
+                        colPosition--;
+                        if (!isValidPosition(rowPosition, colPosition, pond))
+                        {
+                            colPosition++;
+                            if (collectedBranch.Count > 0)
+                            {
+                                collectedBranch.Pop();
+                            }
+                        }
+                        else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                        {
+                            branchesCount--;
+                            collectedBranch.Push(pond[rowPosition, colPosition]);
+                            pond[rowPosition, colPosition] = '-';
+
+                        }
+                        else if (pond[rowPosition, colPosition] == 'F')
+                        {
+                            collectedFish++;
+                            pond[rowPosition, colPosition] = '-';
+                            colPosition = 0;
+
+                            if (pond[rowPosition, colPosition] == 'F')
+                            {
+                                collectedFish++;
+                                pond[rowPosition, colPosition] = '-';
+                                colPosition = pond.GetLength(1)-1;
+                                if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                                {
+                                    branchesCount--;
+                                    collectedBranch.Push(pond[rowPosition, colPosition]);
+                                    pond[rowPosition, colPosition] = '-';
+                                }
+                                pond[rowPosition, colPosition] = '-';
+                            }
+                            else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                            {
+                                branchesCount--;
+                                collectedBranch.Push(pond[rowPosition, colPosition]);
+                                pond[rowPosition, colPosition] = '-';
+
+                            }
+                        }
+                        break;
+                    case "right":
+                        colPosition++;
+                        if (!isValidPosition(rowPosition, colPosition, pond))
+                        {
+                            colPosition--;
+                            if (collectedBranch.Count > 0)
+                            {
+                                collectedBranch.Pop();
+                            }
+                        }
+                        else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                        {
+                            branchesCount--;
+                            collectedBranch.Push(pond[rowPosition, colPosition]);
+                            pond[rowPosition, colPosition] = '-';
+
+                        }
+                        else if (pond[rowPosition, colPosition] == 'F')
+                        {
+                            collectedFish++;
+                            pond[rowPosition, colPosition] = '-';
+                            colPosition = pond.GetLength(1)-1;
+
+                            if (pond[rowPosition, colPosition] == 'F')
+                            {
+                                collectedFish++;
+                                pond[rowPosition, colPosition] = '-';
+                                colPosition = 0;
+                                if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                                {
+                                    branchesCount--;
+                                    collectedBranch.Push(pond[rowPosition, colPosition]);
+                                    pond[rowPosition, colPosition] = '-';
+                                }
+                                pond[rowPosition, colPosition] = '-';
+                            }
+                            else if (char.IsLetter(pond[rowPosition, colPosition]) && char.IsLower(pond[rowPosition, colPosition]))
+                            {
+                                branchesCount--;
+                                collectedBranch.Push(pond[rowPosition, colPosition]);
+                                pond[rowPosition, colPosition] = '-';
+
+                            }
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+            pond[rowPosition, collectedFish] = 'B';
+            if (branchesCount==0)
+            {
+                Console.WriteLine($"The Beaver successfully collect {collectedBranch.Count} wood branches: {string.Join(", ", collectedBranch.Reverse())}");
+            }
+            else
+            {
+                Console.WriteLine($"The Beaver failed to collect every wood branch. There are {branchesCount} branches left.");
+            }
+
+
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write(pond[rowPosition,col]+" ");
+                }
+                Console.WriteLine();
+            }
+
+            
+
+        }
+        static bool isValidPosition(int rowPosition, int colPosition, char[,] pond)
+        {
+            return rowPosition >= 0
+                && rowPosition < pond.GetLength(0)
+                && colPosition >= 0
+                && colPosition < pond.GetLength(1);
+        }
+    }
+}
---
+++ Advanced/ExamPreprationOlds/FishingNet/Net.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        private readonly List<Fish> fish;
+
+        public Net(string material, int capacity)
+        {
+            fish = new List<Fish>();
+
+            Material = material;
+            Capacity = capacity;
+        }
+
+        public List<Fish> Fish { get { return this.fish; } }
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count => fish.Count;
+        
+
+        public string AddFish(Fish fish) //adds a Fish to the fish's collection if there is room for it. Before adding a fish, check:
+        {
+            //If the fish type is null or whitespace.
+            //If the fish’s length or weight is zero or less.
+            //If the fish type, length, or weight properties are not valid, return: "Invalid fish."
+            if (string.IsNullOrWhiteSpace(fish.FishType) || fish.Length<=0 || fish.Weight<=0)
+            {
+                return "Invalid fish.";
+            }
+            //If the net is full (there is no room for more fish), return "Fishing net is full.". 
+            else if (Capacity==Count)
+            {
+                return "Fishing net is full.";
+            }
+            //Otherwise, return: "Successfully added {fishType} to the fishing net."
+            this.fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+        public bool ReleaseFish(double weight) // removes a fish by given weight, if such exists return true, otherwise false.
+        {
+            return fish.Remove(fish.First(x => x.Weight == weight));
+        }
+
+        public Fish GetFish(string fishType) // search and returns a fish by given fish type.
+        {
+            return fish.FirstOrDefault(x=>x.FishType==fishType);
+        }
+        public Fish GetBiggestFish()// search and returns the longest fish in the collection.{
+        {
+            return fish.OrderByDescending(x=>x.Weight).First();
+        }
+        public string Report() // returns information about the Net and all fish that were not released, order by fish's length descending  in the following format:	
+        {
+            var orderedFishes = fish.OrderByDescending(x => x.Length);
+            return $"Into the {Material}:{Environment.NewLine}{string.Join(Environment.NewLine, orderedFishes)}.";
+            
+        }
+    }
+}
