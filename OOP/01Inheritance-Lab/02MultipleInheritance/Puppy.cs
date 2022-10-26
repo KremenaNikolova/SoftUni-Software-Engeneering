28784265f4658ec76ba19918c18a41bf0d348009
KremenaNikolova <aradia1992@gmail.com>
2022-10-26 20:36:55 +0300

    Initial commit

---
+++ OOP/01Inheritance-Lab/02MultipleInheritance/Puppy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    internal class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
