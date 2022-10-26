aa37f572fefd8ee86efafec3a37625a664e86094
KremenaNikolova <aradia1992@gmail.com>
2022-10-26 20:58:13 +0300

    Initial commit

---
+++ OOP/01Inheritance-Lab/04RandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    internal class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            int index = random.Next(this.Count);
+            string removed = this[index];
+            RemoveAt(index);
+            return removed;
+
+        }
+    }
+}
---
+++ OOP/01Inheritance-Lab/04RandomList/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("kremena");
+            randomList.Add("valerieva");
+            randomList.Add("nikolova");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
