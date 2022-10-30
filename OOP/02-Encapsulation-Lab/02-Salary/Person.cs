0557ff7a61682881c81eb2f3fba42917b5ed7681
KremenaNikolova <aradia1992@gmail.com>
2022-10-30 11:03:46 +0200

    Initial commit

---
+++ OOP/02-Encapsulation-Lab/02-Salary/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            //Read the percentage of the bonus to every Person's salary. People younger than 30 get half the increase. 
+            if (Age<30)
+            {
+                Salary += Salary * (percentage / 200);
+            }
+            Salary += Salary * percentage/100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
