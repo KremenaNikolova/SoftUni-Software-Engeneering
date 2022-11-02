19043a01c1043e8b90c0698fdd66667546d7b547
Kremena Nikolova <aradia1992@gmail.com>
2022-11-02 23:37:42 +0200

    Initial commit

---
+++ OOP/03-Interfaces-And-Abstraction-Exercise/02-Multiple-Implementation/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_Multiple_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
