cc663fe4f808bc65ae6692009981452d0422d8c2
Kremena Nikolova <aradia1992@gmail.com>
2022-11-04 09:16:22 +0200

    Initial commit

---
+++ OOP/03-Interfaces-And-Abstraction-Exercise/05-Birthday-Celebrations/Citizens.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04_Border_Control
+{
+    public class Citizens : IIdentify
+    {
+        public Citizens(string name, int age, string id)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Id { get; private set; }
+
+        public void ChechID(string checkID)
+        {
+            string lastDigits = Id.Substring(Id.Length - checkID.Length);
+            for (int i = 0; i < checkID.Length; i++)
+            {
+                if (lastDigits[i] != checkID[i])
+                {
+                    return;
+                }
+            }
+            Console.WriteLine(Id);
+        }
+    }
+}
