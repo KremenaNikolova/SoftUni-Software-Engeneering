fcedc5b8320c8e896d9b5b807601a380e521e481
Kremena Nikolova <aradia1992@gmail.com>
2022-11-06 23:14:28 +0200

    Initial commit

---
+++ OOP/04-OOP-Polymorphism-Lab/03-Shapes/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height, width;
+
+        public Rectangle(double height, double width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return height * width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2*height+2*width;
+        }
+        public override string Draw()
+        {
+            return base.Draw() + GetType().Name;
+        }
+    }
+}
