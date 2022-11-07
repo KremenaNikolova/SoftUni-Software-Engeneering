c3dd620f0c6daad4b4f4cbf887a9cc0647a6aaf4
Kremena Nikolova <aradia1992@gmail.com>
2022-11-07 18:56:21 +0200

    Not ready

---
+++ OOP/04-Polymorphism-Exercise/02-Vehicles-Extension/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Vehicles
+{
+    public class Truck : Vehicles
+
+    {
+        private const double CONSUMPTION_INCREASE = 1.6;
+        public Truck(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption + CONSUMPTION_INCREASE) {}
+
+        public override void Driving(double distance)
+        {
+            base.Driving(distance);
+        }
+        public override void Refueling(double amountOfFuel)
+        {
+            amountOfFuel -= amountOfFuel * 0.05;
+            base.Refueling(amountOfFuel);
+            
+        }
+    }
+}
---
+++ OOP/04-Polymorphism-Exercise/02-Vehicles-Extension/Vehicles.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _01_Vehicles
+{
+    public abstract class Vehicles
+    {
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        protected Vehicles(double fuelQuantity, double fuelConsumption)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
+
+        public virtual void Driving(double distance)
+        {
+            double fuelAmount = fuelQuantity;
+            if ((fuelAmount -= distance * fuelConsumption) > 0)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+                Console.WriteLine($"{GetType().Name} travelled {distance} km");
+                return;
+            }
+            Console.WriteLine($"{GetType().Name} needs refueling");
+        }
+        public virtual void Refueling(double amountOfFuel)
+        {
+            fuelQuantity += amountOfFuel;
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name}: {fuelQuantity:f2}";
+        }
+
+
+    }
+}
