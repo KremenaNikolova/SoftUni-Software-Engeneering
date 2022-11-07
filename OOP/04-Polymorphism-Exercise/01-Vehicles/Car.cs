49a94e04329de43e5965680b6dee42b32010a8c2
Kremena Nikolova <aradia1992@gmail.com>
2022-11-07 18:24:04 +0200

    Initial commit

---
+++ OOP/04-Polymorphism-Exercise/01-Vehicles/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_Vehicles
+{
+    public class Car : Vehicles
+    {
+        private const double CONSUMPTION_INCREASE = 0.9;
+
+        public Car(double fuelQuantity, double fuelConsumption) : base(fuelQuantity, fuelConsumption + CONSUMPTION_INCREASE) { }
+
+        public override void Driving(double distance)
+        {
+            base.Driving(distance);
+        }
+        public override void Refueling(double amountOfFuel)
+        {
+            base.Refueling(amountOfFuel);
+        }
+    }
+}
---
+++ OOP/04-Polymorphism-Exercise/01-Vehicles/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _01_Vehicles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carInformation = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] truckInformation = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Vehicles car = new Car(double.Parse(carInformation[1]), double.Parse(carInformation[2]));
+            Vehicles truck = new Truck(double.Parse(truckInformation[1]), double.Parse(truckInformation[2]));
+
+            int numOfCommands = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] action = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string command = action[0];
+                string carOrTruck = action[1];
+                double distanceOrLitters = double.Parse(action[2]);
+
+                if (command == "Drive")
+                {
+                    if (carOrTruck=="Car")
+                    {
+                        car.Driving(distanceOrLitters);
+                    }
+                    else
+                    {
+                        truck.Driving(distanceOrLitters);
+                    }
+                }
+                else if (command== "Refuel")
+                {
+                    if (carOrTruck=="Car")
+                    {
+                        car.Refueling(distanceOrLitters);
+                    }
+                    else
+                    {
+                        truck.Refueling(distanceOrLitters);
+                    }
+                }
+            }
+
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+        }
+    }
+}
