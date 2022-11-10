d69af77c55f9a52d3c1f5ab22ee378c1e8a236a2
Kremena Nikolova <aradia1992@gmail.com>
2022-11-10 19:25:41 +0200

    Initial commit

---
+++ OOP/05-Exceptions-and-Error-Handling-Lab/01-Square-Root/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+namespace _01_Square_Root
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                double input = double.Parse(Console.ReadLine());
+                if (input<0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(input));
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
