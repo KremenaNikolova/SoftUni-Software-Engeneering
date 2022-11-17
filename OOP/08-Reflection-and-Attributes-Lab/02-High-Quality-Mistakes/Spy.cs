dc3f1593c2b70e0ba591f494ea278f93ce24191b
Kremena Nikolova <aradia1992@gmail.com>
2022-11-17 13:28:39 +0200

    Initial commit

---
+++ OOP/08-Reflection-and-Attributes-Lab/02-High-Quality-Mistakes/Spy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string classInvestigate, params string[] fieldsInvestigate)
+        {
+            Type classType = Type.GetType(classInvestigate);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            StringBuilder sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {classInvestigate}");
+            foreach (FieldInfo field in classFields.Where(f=>fieldsInvestigate.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            MethodInfo[] classMethod = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] nonPublicMethod = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+            object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            foreach (var field in classFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            foreach (var method in classMethod.Where(x => x.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+            foreach (var method in nonPublicMethod.Where(x=>x.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+
+    
+}
---
+++ OOP/08-Reflection-and-Attributes-Lab/02-High-Quality-Mistakes/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy= new Spy();
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            //Console.WriteLine(result);
+
+            string analazyResult = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(analazyResult);
+        }
+    }
+}
