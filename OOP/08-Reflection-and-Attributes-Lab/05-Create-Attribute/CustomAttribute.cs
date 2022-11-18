7a56d230233a51e7778b584f6481af8c0dd07fd3
Kremena Nikolova <aradia1992@gmail.com>
2022-11-18 19:15:24 +0200

    Initial commit

---
+++ OOP/08-Reflection-and-Attributes-Lab/05-Create-Attribute/CustomAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Author : Attribute
+    {
+        public Author(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ OOP/08-Reflection-and-Attributes-Lab/05-Create-Attribute/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Kremena")]
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
