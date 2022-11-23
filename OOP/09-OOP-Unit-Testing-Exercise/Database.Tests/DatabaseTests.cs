3b8a1e75e6072df9f6c17b009f5195372b40806c
Kremena Nikolova <aradia1992@gmail.com>
2022-11-23 21:28:10 +0200

    Initial commit

--- OOP/09-OOP-Unit-Testing-Exercise/Database.Tests/DatabaseTests.cs
+++ OOP/09-OOP-Unit-Testing-Exercise/Database.Tests/DatabaseTests.cs
@@ -1,6 +1,7 @@
 namespace Database.Tests
 {
     using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
     using System;
 
     [TestFixture]
@@ -18,15 +19,41 @@ namespace Database.Tests
             Assert.AreEqual(expextedData, actualData);
         }
 
+        [Test]
         public void TestShouldThrowExceptionIfDataCountIs16AndAbove()
         {
             int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
 
-            Assert.Throws<InvalidOperationException>(()=>
+            Assert.Throws<InvalidOperationException>(() =>
             {
                 Database db = new Database(data);
             }, "Array's capacity must be exactly 16 integers!");
 
+        }
+
+        [Test]
+        public void TestRemoveShouldRemoveItemsPropetly()
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            Database db = new Database(data);
+
+            db.Remove();
+            db.Remove();
+            int exectedDataCount = data.Length - 2;
+            int actualDataCount = db.Count;
+
+            Assert.AreEqual(exectedDataCount, actualDataCount);
+        }
+
+        [Test]
+        public void TestRemoveShouldThrowExceptionIfCountIsZeroOrBelow()
+        {
+            Database db = new Database();
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                db.Remove();
+            }, "The collection is empty!");
         }
     }
 }
