339a0798c293078738c69d3cd78d0e706c939fe8
Kremena Nikolova <aradia1992@gmail.com>
2022-12-18 18:48:14 +0200

    Initial commit

---
+++ OOP/12-DI-Workshop/SoftUniDI-Framework/Attribute/Attribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.Attribute
+{
+    public class Attribute
+    {
+    }
+}
---
+++ OOP/12-DI-Workshop/SoftUniDI-Framework/Attribute/Inject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniDI_Framework.Attribute
+{
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Field)]
+    public class Inject : Attribute
+    {
+    }
+}
