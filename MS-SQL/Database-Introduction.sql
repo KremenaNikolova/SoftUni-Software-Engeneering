063e984752efaa738070b2247cc0e1be46eadcf6
Kremena Nikolova <aradia1992@gmail.com>
2023-01-16 23:15:42 +0200

    Initial commit

--- MS-SQL/Database-Introduction.sql
+++ MS-SQL/Database-Introduction.sql
@@ -491,9 +491,56 @@ SELECT * FROM [Employees]
 
 
 --20-Basic Select All Fields and Order Them
+SELECT * FROM [Towns]
+ORDER BY [Name]
+
+SELECT * FROM [Departments]
+ORDER BY [Name]
+
+SELECT * FROM [Employees]
+ORDER BY [Salary] DESC
+
+
+--21-Basic Select Some Fields
+SELECT [Name] 
+FROM [Towns] 
+ORDER BY [Name]
+
+SELECT [Name] 
+FROM [Departments] 
+ORDER BY [Name]
+
+SELECT [FirstName], [LastName], [JobTitle], [Salary] 
+FROM [Employees] 
+ORDER BY [Salary] DESC
+
+
+--22-Increase Employees Salary
+USE[SoftUni]
+GO
+
+UPDATE [Employees]
+SET [Salary] = [Salary]+([Salary]*0.1)
+
+SELECT [Salary] FROM [Employees]
+
+
+--23--Decrease Tax Rate
+USE [Hotel]
+GO
+
+UPDATE [Payments]
+SET [TaxRate] = [TaxRate]-([TaxRate]*0.03)
+
+SELECT [TaxRate] FROM [Payments]
 
 
 
+--24--Delete All Records
+USE [Hotel]
+GO
+
+DELETE [Occupancies]
 
 
 
