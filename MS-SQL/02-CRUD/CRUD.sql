ae7748f1dd834131d79e254df9df4f2c4884ea09
Kremena Nikolova <aradia1992@gmail.com>
2023-01-18 16:41:06 +0200

    Initial commit

---
+++ MS-SQL/02-CRUD/CRUD.sql
@@ -0,0 +1,19 @@
+--02-Find All Information About Departments
+USE SoftUni
+GO
+
+SELECT * 
+  FROM Departments
+
+--03-Find all Department Names
+SELECT [Name]
+  FROM Departments
+
+
+--04-Find Salary of Each Employee
+SELECT FirstName, LastName, Salary
+  FROM Employees
+
+--05-Find Full Name of Each Employee
+
+
