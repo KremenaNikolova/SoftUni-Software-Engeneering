76c3884bead35af65fddb13ad5a6d398c336d10c
Kremena Nikolova <aradia1992@gmail.com>
2023-01-20 23:01:20 +0200

    Initial commit

--- MS-SQL/03-Table-Relations/Table-Relations.sql
+++ MS-SQL/03-Table-Relations/Table-Relations.sql
@@ -17,3 +17,18 @@ CREATE TABLE Persons
 			 Salary DECIMAL(9,2),
 			 PassportID INT FOREIGN KEY REFERENCES Passports(PersonID) UNIQUE
 )
+
+INSERT INTO Passports(PassportNumber)
+VALUES
+       ('N34FG21B'),
+	   ('K65LO4R7'),
+	   ('ZE657QP2')
+
+INSERT INTO Persons(FirstName, Salary, PassportID)
+VALUES
+       ('Roberto', 43300.00, 102),
+	   ('Tom', 56100.00, 103),
+	   ('Yana', 60200.00, 101)
+
+
+--02. One-To-Many Relationship
