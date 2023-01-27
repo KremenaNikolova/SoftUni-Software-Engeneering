4503143b8eae91149002a0e5fbd1022f1c68681d
Kremena Nikolova <aradia1992@gmail.com>
2023-01-27 12:00:43 +0200

    Initial commit

--- MS-SQL/05-Subqueries-and-JOINs/Subqueries-and-JOINs.sql
+++ MS-SQL/05-Subqueries-and-JOINs/Subqueries-and-JOINs.sql
@@ -8,7 +8,8 @@ SELECT TOP 5
 	,e.AddressId
 	,a.AddressText
 FROM Employees AS e
-JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Addresses AS a 
+ON e.AddressID = a.AddressID
 ORDER BY e.AddressID
 
 
@@ -19,7 +20,62 @@ SELECT TOP 50
 	,t.[Name] AS Town
 	,a.AddressText
 FROM Employees AS e
-JOIN Addresses AS a ON e.AddressID = a.AddressID
+JOIN Addresses AS a 
+ON e.AddressID = a.AddressID
 JOIN Towns AS t ON a.TownID = t.TownID
 ORDER BY e.FirstName, e.LastName
 
+
+--03. Sales Employees
+SELECT e.EmployeeID
+       ,e.FirstName
+	   ,e.LastName
+	   ,d.[Name] AS DepartmentName
+FROM Employees AS e
+JOIN Departments AS d 
+ON e.DepartmentID = d.DepartmentID
+WHERE d.[Name] = 'Sales'
+ORDER BY e.EmployeeID
+
+
+--04. Employee Departments
+SELECT TOP (5)
+        e.EmployeeID
+       ,e.FirstName
+	   ,e.Salary
+	   ,d.[Name] AS DepartmentName
+FROM Employees AS e
+JOIN Departments AS d 
+ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY e.DepartmentID
+
+
+--05. Employees Without Projects
+SELECT TOP (3)
+     e.EmployeeID
+	,e.FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep 
+ON e.EmployeeID = ep.EmployeeID
+WHERE ep.ProjectID IS NULL
+ORDER BY e.EmployeeID
+
+
+--06. Employees Hired After
+SELECT 
+     e.FirstName
+	,e.LastName
+	,e.HireDate
+	,d.[Name] AS DeptName
+FROM Employees AS e
+JOIN Departments AS d 
+ON e.DepartmentID = d.DepartmentID AND d.[Name] IN ('Finance', 'Sales')
+WHERE e.HireDate > '1999-1-1'
+ORDER BY e.HireDate
+
+
+
+select * from Projects
+select * from EmployeesProjects
+select * from Employees
