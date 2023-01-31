2f372816d7232eaa7cf7d5a2cca6e23b6420de35
Kremena Nikolova <aradia1992@gmail.com>
2023-01-31 13:49:32 +0200

    Initial commit

--- MS-SQL/06-Indices-and-Data-Aggregation/Indices-and-Data-Aggregation.sql
+++ MS-SQL/06-Indices-and-Data-Aggregation/Indices-and-Data-Aggregation.sql
@@ -30,3 +30,56 @@ ORDER BY AVG([wz].[MagicWandSize])
 
 
 --05. Deposits Sum
+  SELECT [DepositGroup],
+		 SUM([DepositAmount])
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup]
+
+
+--06. Deposits Sum for Ollivander Family
+  SELECT [DepositGroup],
+		 SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+   WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [DepositGroup]
+
+
+--07. Deposits Filter
+  SELECT [DepositGroup],
+		 SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+   WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [DepositGroup]
+  HAVING SUM([DepositAmount]) < 150000
+ORDER BY [TotalSum] DESC
+
+
+--08. Deposit Charge
+  SELECT [DepositGroup],
+		 [MagicWandCreator],
+		 MIN([DepositCharge]) AS [MinDepositeCharge]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup], 
+		 [MagicWandCreator]
+ORDER BY [MagicWandCreator],
+		 [DepositGroup]
+
+
+--09. Age Groups
+  SELECT [AgeGroup],
+		 COUNT(*) AS [WizardCount]
+    FROM
+         (
+	      SELECT 
+	    	    CASE
+	    		    WHEN [Age] BETWEEN 0 AND 10 THEN '[0-10]'
+	    		    WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+	    		    WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+	    		    WHEN [Age] BETWEEN 31 AND 40 THEN '[31-40]'
+	    		    WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+	    		    ELSE '[61+]'
+	    		 END AS [AgeGroup]
+	        FROM [WizzardDeposits]
+	      )   AS [AgeGroupSubquery]
+GROUP BY [AgeGroup]
+
