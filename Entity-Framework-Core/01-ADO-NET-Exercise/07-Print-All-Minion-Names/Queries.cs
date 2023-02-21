adfdf9be7744a641f9b46a4d1a1ac8a2c11a95f8
Kremena Nikolova <aradia1992@gmail.com>
2023-02-21 22:57:58 +0200

    Initial commit

---
+++ Entity-Framework-Core/01-ADO-NET-Exercise/07-Print-All-Minion-Names/Queries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Print_All_Minion_Names
+{
+    public static class Queries
+    {
+        public const string MinionsName = "SELECT Name FROM Minions";
+    }
+}
---
+++ Entity-Framework-Core/01-ADO-NET-Exercise/07-Print-All-Minion-Names/StrtUp.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace _07_Print_All_Minion_Names
+{
+    internal class StrtUp
+    {
+        static async Task Main(string[] args)
+        {
+            await using SqlConnection connection = new SqlConnection(Config.ConnectionString);
+            await connection.OpenAsync();
+
+
+
+        }
+    }
+}
