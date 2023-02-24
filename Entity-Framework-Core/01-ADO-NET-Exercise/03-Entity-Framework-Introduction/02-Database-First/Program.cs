c027ef82028e249ea224b64715d1d26725cc0a57
Kremena Nikolova <aradia1992@gmail.com>
2023-02-24 10:43:46 +0200

    Initial commit

---
+++ Entity-Framework-Core/01-ADO-NET-Exercise/03-Entity-Framework-Introduction/02-Database-First/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Database_First
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
