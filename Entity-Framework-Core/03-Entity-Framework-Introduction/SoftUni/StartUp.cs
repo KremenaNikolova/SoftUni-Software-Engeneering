9f401c7d714302f0b5b4ba9b75610047b4c1149e
Kremena Nikolova <aradia1992@gmail.com>
2023-02-25 16:31:12 +0200

    Initial commit

--- Entity-Framework-Core/03-Entity-Framework-Introduction/SoftUni/StartUp.cs
+++ Entity-Framework-Core/03-Entity-Framework-Introduction/SoftUni/StartUp.cs
@@ -10,11 +10,14 @@ public class StartUp
     {
         SoftUniContext dbContext = new SoftUniContext();
 
-        string output = GetEmployeesFullInformation(dbContext);
+        //string output = GetEmployeesFullInformation(dbContext); -Problem 03
+
+        string output = GetEmployeesWithSalaryOver50000(dbContext);
 
         Console.WriteLine(output);
     }
 
+    //03. Employees Full Information
     public static string GetEmployeesFullInformation(SoftUniContext context)
     {
         StringBuilder sb = new StringBuilder();
@@ -34,6 +37,29 @@ public class StartUp
         foreach (var employee in employees)
         {
             sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+
+    //04. Employees with Salary Over 50 000
+    public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .Where(e => e.Salary > 50000)
+            .OrderBy(e => e.FirstName)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
         }
 
         return sb.ToString().TrimEnd();
