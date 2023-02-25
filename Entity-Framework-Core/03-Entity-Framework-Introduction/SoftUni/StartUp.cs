7799d8b3f3ba7dbebb5c332d09ce5cefe5d01582
Kremena Nikolova <aradia1992@gmail.com>
2023-02-25 15:56:46 +0200

    Initial commit

---
+++ Entity-Framework-Core/03-Entity-Framework-Introduction/SoftUni/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        SoftUniContext dbContext = new SoftUniContext();
+
+        string output = GetEmployeesFullInformation(dbContext);
+
+        Console.WriteLine(output);
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext context)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        var employees = context.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToArray();
+
+        foreach (var employee in employees)
+        {
+            sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
