c8d674673cd7cb506ee51fed30f1095f9e504ba4
Kremena Nikolova <aradia1992@gmail.com>
2023-03-01 22:41:59 +0200

    Initial commit

--- Entity-Framework-Core/04-Entity-Relations/Student-System/P01_StudentSystem.Data.Common/Config.cs
+++ Entity-Framework-Core/04-Entity-Relations/Student-System/P01_StudentSystem.Data.Common/Config.cs
@@ -2,6 +2,6 @@
 {
     public static class DbConfig
     {
-        public static string ConnectionString = @"Server=ARADIA;Database=Bet366;Integrated Security=True;TrustServerCertificate=True;";
+        public static string ConnectionString = @"Server=ARADIA;Database=Alohomora;Integrated Security=True;TrustServerCertificate=True;";
     }
 }
