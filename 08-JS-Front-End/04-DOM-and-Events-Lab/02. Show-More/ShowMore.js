284901fdf27710e326b103b0ea911ec43b021388
Kremena Nikolova <aradia1992@gmail.com>
2023-05-08 18:15:17 +0300

    Initial commit

--- 08-JS-Front-End/04-DOM-and-Events-Lab/02. Show-More/ShowMore.js
+++ 08-JS-Front-End/04-DOM-and-Events-Lab/02. Show-More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+    const readMore = document.getElementById('more');
+    readMore.style.display = 'none';
+
+    const moreText = document.getElementById('text');
+    moreText.style.display = 'inline';
 }
