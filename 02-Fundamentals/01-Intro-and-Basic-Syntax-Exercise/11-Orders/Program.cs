2fc3d1b1c3923de05a6933cece4b011ce1383a4a
Kremena Nikolova <aradia1992@gmail.com>
2023-05-17 10:20:52 +0300

    Initial commit

---
+++ 02-Fundamentals/01-Intro-and-Basic-Syntax-Exercise/11-Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _11_Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ordersCount = int.Parse(Console.ReadLine());
+
+            decimal total = 0;
+
+            for (int i = 0; i < ordersCount; i++)
+            {
+                decimal capsulePrice = decimal.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsulesCount = int.Parse(Console.ReadLine());
+
+                decimal currTotal = (days * capsulesCount) * capsulePrice;
+                total += currTotal;
+                Console.WriteLine($"The price for the coffee is: ${currTotal:f2}");
+            }
+
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
