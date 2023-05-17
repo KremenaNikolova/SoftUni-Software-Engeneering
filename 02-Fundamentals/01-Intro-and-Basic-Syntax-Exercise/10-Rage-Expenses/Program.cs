d03be8b8d0932ed35b7957b398a105c8c19204c4
Kremena Nikolova <aradia1992@gmail.com>
2023-05-17 10:06:30 +0300

    Initial commit

--- 02-Fundamentals/01-Intro-and-Basic-Syntax-Exercise/10-Rage-Expenses/Program.cs
+++ 02-Fundamentals/01-Intro-and-Basic-Syntax-Exercise/10-Rage-Expenses/Program.cs
@@ -6,7 +6,44 @@ namespace _10_Rage_Expenses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lostGames = int.Parse(Console.ReadLine());
+
+            decimal headsetPrice = decimal.Parse(Console.ReadLine());
+            decimal mousePrice = decimal.Parse(Console.ReadLine());
+            decimal keyboardPrice = decimal.Parse(Console.ReadLine());
+            decimal displayPrice = decimal.Parse(Console.ReadLine());
+
+            int headsetTrashes = 0;
+            int mouseTrashes = 0;
+            int keyboardTrashes = 0;
+            int displayTrashes = 0;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    headsetTrashes++;
+                }
+
+                if (i % 3 == 0)
+                {
+                    mouseTrashes++;
+                }
+
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    keyboardTrashes++;
+
+                    if (keyboardTrashes % 2 == 0)
+                    {
+                        displayTrashes++;
+                    }
+                }
+            }
+
+            decimal totalCost = headsetTrashes * headsetPrice + mouseTrashes * mousePrice + keyboardTrashes * keyboardPrice + displayTrashes * displayPrice;
+
+            Console.WriteLine($"Rage expenses: {totalCost:f2} lv.");
         }
     }
 }
