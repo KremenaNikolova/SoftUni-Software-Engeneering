2694fb3c5f2299115974c607318df879421395cd
Kremena Nikolova <aradia1992@gmail.com>
2023-05-26 09:38:43 +0300

    Initial commit

---
+++ 09-ASP.NET-Fundamentals/01-MVC-Intro/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using _01_MVC_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _01_MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Home Page";
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is an ASP.NET Core MVC app.";
+            return View();
+        }
+
+        public IActionResult Numbers()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 09-ASP.NET-Fundamentals/01-MVC-Intro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01_MVC_Intro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
