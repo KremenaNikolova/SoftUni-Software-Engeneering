4dadec66ddced8faea2022d4339bcf5ae7ade88d
Kremena Nikolova <aradia1992@gmail.com>
2023-06-09 14:27:13 +0300

    Define Entities

---
+++ 09-ASP.NET-Fundamentals/TaskBoard-App-Workshop/TaskBoardApp/Data/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static TaskBoardApp.Data.DataConstans;
+
+namespace TaskBoardApp.Data.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(DataConstans.)]
+        public string Title { get; set; } = null!;
+    }
+}
