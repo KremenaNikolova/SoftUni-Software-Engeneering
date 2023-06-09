8d19ed875dbf85a28be11b07008437dc5a260910
Kremena Nikolova <aradia1992@gmail.com>
2023-06-09 14:27:52 +0300

    Define Entities

--- 09-ASP.NET-Fundamentals/TaskBoard-App-Workshop/TaskBoardApp/Data/Models/Board.cs
+++ 09-ASP.NET-Fundamentals/TaskBoard-App-Workshop/TaskBoardApp/Data/Models/Board.cs
@@ -1,6 +1,24 @@
-﻿namespace TaskBoardApp.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+using static TaskBoardApp.Data.DataConstans.Board;
+
+namespace TaskBoardApp.Data.Models
 {
     public class Board
     {
+        public Board()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(BoardMaxName)]
+        [MinLength(BoardMinName)]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Task>? Tasks { get; set; }
     }
 }
