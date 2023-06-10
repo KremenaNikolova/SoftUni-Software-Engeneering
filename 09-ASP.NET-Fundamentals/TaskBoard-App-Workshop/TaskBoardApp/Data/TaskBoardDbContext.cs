f78095c9bcf38b842160d9db278ba8782a89d701
Kremena Nikolova <aradia1992@gmail.com>
2023-06-10 08:38:44 +0300

    Seed the Database

--- 09-ASP.NET-Fundamentals/TaskBoard-App-Workshop/TaskBoardApp/Data/TaskBoardDbContext.cs
+++ 09-ASP.NET-Fundamentals/TaskBoard-App-Workshop/TaskBoardApp/Data/TaskBoardDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TaskBoardApp.Data.Models;
 using Task = TaskBoardApp.Data.Models.Task;
@@ -7,28 +8,140 @@ namespace TaskBoardApp.Data
 {
     public class TaskBoardDbContext : IdentityDbContext
     {
+        private IdentityUser TestUser { get; set; } = null!;
+
+        private Board OpenBoard { get; set; } = null!;
+
+        private Board InProgressBoard { get; set; } = null!;
+
+        private Board DoneBoard { get; set; } = null!;
+
+
         public TaskBoardDbContext(DbContextOptions<TaskBoardDbContext> options)
             : base(options)
         {
-            //I have task to implement database.migrate but i dont want to do it and thats why this is in comment
-
             //Database.Migrate();
         }
+
 
         public DbSet<Task> Tasks { get; set; } = null!;
 
         public DbSet<Board> Boards { get; set; } = null!;
 
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder
                 .Entity<Task>()
-                .HasOne(t=>t.Board)
-                .WithMany(t=>t.Tasks)
-                .HasForeignKey(t=>t.BoardId)
+                .HasOne(t => t.Board)
+                .WithMany(t => t.Tasks)
+                .HasForeignKey(t => t.BoardId)
                 .OnDelete(DeleteBehavior.Restrict);
 
+            builder
+                .Entity<Task>()
+                .HasData(GenerateTask());
+
+            SeedUser();
+            builder
+                .Entity<IdentityUser>()
+                .HasData(TestUser);
+
+            SeedBoard();
+            builder
+                .Entity<Board>()
+                .HasData(OpenBoard, InProgressBoard, DoneBoard);
+
+
             base.OnModelCreating(builder);
+        }
+
+        private void SeedUser()
+        {
+            var hasher = new PasswordHasher<IdentityUser>();
+
+            TestUser = new IdentityUser()
+            {
+                UserName = "pesho@abv.bg",
+                NormalizedEmail = "PESHO@ABV.BG"
+            };
+
+            TestUser.PasswordHash = hasher.HashPassword(TestUser, "123456");
+        }
+
+
+        private void SeedBoard()
+        {
+            OpenBoard = new Board()
+            {
+                Id = 1,
+                Name = "Open"
+            };
+
+            InProgressBoard = new Board()
+            {
+                Id = 2,
+                Name = "In progress"
+            };
+
+            DoneBoard = new Board()
+            {
+                Id = 3,
+                Name = "Done"
+            };
+        }
+
+
+        private Task[] GenerateTask()
+        {
+            ICollection<Task> tasks = new HashSet<Task>();
+            Task currTask;
+
+            currTask = new Task()
+            {
+                Id = 1,
+                Title = "Improve CSS styles",
+                Description = "Implement better styling for all public pages",
+                CreateOn = DateTime.Now.AddDays(-200),
+                OwnerId = TestUser.Id,
+                BoardId = OpenBoard.Id
+            };
+            tasks.Add(currTask);
+
+            currTask = new Task()
+            {
+                Id = 2,
+                Title = "Android Client App",
+                Description = "Create Android client app for the TaskBoard RESTful API",
+                CreateOn = DateTime.Now.AddMonths(-5),
+                OwnerId = TestUser.Id,
+                BoardId = OpenBoard.Id
+            };
+            tasks.Add(currTask);
+
+            currTask = new Task()
+            {
+                Id = 3,
+                Title = "Desktop Client App",
+                Description = "Create Windows Forms desktop app client for the TaskBoard RESTful API",
+                CreateOn = DateTime.Now.AddMonths(-2),
+                OwnerId = TestUser.Id,
+                BoardId = InProgressBoard.Id
+            };
+            tasks.Add(currTask);
+
+            currTask = new Task()
+            {
+                Id = 4,
+                Title = "Create Tasks",
+                Description = "Implement [Create Task] page for adding new tasks",
+                CreateOn = DateTime.Now.AddYears(-1),
+                OwnerId = TestUser.Id,
+                BoardId = DoneBoard.Id
+            };
+            tasks.Add(currTask);
+
+            return tasks.ToArray();
         }
     }
 }
