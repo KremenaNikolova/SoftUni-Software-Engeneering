42d1eca610ee44b50a0f75df53e6327f2918134b
Kremena Nikolova <aradia1992@gmail.com>
2023-11-05 11:23:06 +0200

    initial commit

---
+++ 08-JS-Front-End/02-JS Arrays-and-Strings-Exercises/1-ArrayRotation.js
@@ -0,0 +1,12 @@
+function solve(array, rotations) {
+  for (let i = 0; i < rotations; i++) {
+    let currElement = array.shift();
+    array.push(currElement);
+  }
+
+  console.log(array.join(" "));
+}
+
+solve([51, 47, 32, 61, 21], 2);
+solve([32, 21, 61, 1], 4);
+solve([2, 4, 15, 31], 5);
