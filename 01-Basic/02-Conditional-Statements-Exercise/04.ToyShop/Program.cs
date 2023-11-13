f85ce4c37315653f2ddbdd69f8080e6997de916d
Kremena Nikolova <aradia1992@gmail.com>
2023-11-13 12:02:31 +0200

    initial commit

--- 01-Basic/02-Conditional-Statements-Exercise/04.ToyShop/Program.cs
+++ 01-Basic/02-Conditional-Statements-Exercise/04.ToyShop/Program.cs
@@ -6,30 +6,24 @@ namespace _04.ToyShop
     {
         static void Main(string[] args)
         {
-            //1.Цена на екскурзията -реално число в интервала[1.00 … 10000.00]
             double priceVacation = double.Parse(Console.ReadLine());
-            //2.Брой пъзели - цяло число в интервала[0… 1000]
             int numPuzzles = int.Parse(Console.ReadLine());
-            //3.Брой говорещи кукли -цяло число в интервала[0 … 1000]
             int numDolls = int.Parse(Console.ReadLine());
-            //4.Брой плюшени мечета -цяло число в интервала[0 … 1000]
             int numBears = int.Parse(Console.ReadLine());
-            //5.Брой миньони - цяло число в интервала[0 … 1000]
             int numMinions = int.Parse(Console.ReadLine());
-            //6.Брой камиончета - цяло число в интервала[0 … 1000]
             int numTrucks = int.Parse(Console.ReadLine());
 
             double totalNum = numBears + numDolls + numMinions + numPuzzles + numTrucks;
-            double totalPrice = numBears*4.10 + numDolls*3 + numMinions*8.2 + numPuzzles*2.6 + numTrucks*2;
+            double totalPrice = numBears * 4.10 + numDolls * 3 + numMinions * 8.2 + numPuzzles * 2.6 + numTrucks * 2;
 
             if (totalNum >= 50)
             {
-                totalPrice = totalPrice - (totalPrice * 0.25);
+                totalPrice -= (totalPrice * 0.25);
             }
 
-            double naem = totalPrice * 0.1;
+            double rent = totalPrice * 0.1;
 
-            double totalEnd = totalPrice - naem - priceVacation;
+            double totalEnd = totalPrice - rent - priceVacation;
 
             if (totalEnd >= 0)
             {
--- 01-Basic/02-Conditional-Statements-Exercise/08.LunchBreak/Program.cs
+++ 01-Basic/02-Conditional-Statements-Exercise/08.LunchBreak/Program.cs
@@ -6,16 +6,11 @@ namespace _08.LunchBreak
     {
         static void Main(string[] args)
         {
-            //1.Име на сериал – текст
             string name = Console.ReadLine();
-            //2.Продължителност на епизод  – цяло число в диапазона[10… 90]
             int time = int.Parse(Console.ReadLine());
-            //3.Продължителност на почивката  – цяло число в диапазона[10… 120]
             int rest = int.Parse(Console.ReadLine());
 
-            //Времето за обяд ще бъде 1/8 от времето за почивка,
             double timeLunch = rest * 1.0 / 8;
-            //а времето за отдих ще бъде 1/4 от времето за почивка. 
             double relaxTime = rest * 1.0 / 4;
 
             double endTime = rest - timeLunch - relaxTime;
@@ -30,12 +25,6 @@ namespace _08.LunchBreak
                 double needTime = time-endTime;
                 Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(needTime)} more minutes.");
             }
-
-            //•	Ако времето е достатъчно да изгледате епизода:
-            //"You have enough time to watch {име на сериал} and left with {останало време} minutes free time."
-            //•	Ако времето не Ви е достатъчно:
-            //"You don't have enough time to watch {име на сериал}, you need {нужно време} more minutes."
-
 
         }
     }
