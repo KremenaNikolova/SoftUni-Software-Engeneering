bc43d4621fb9d764d63deedc0cbf8c3b2fb3e41c
Kremena Nikolova <aradia1992@gmail.com>
2023-11-22 09:44:09 +0200

    initial commit

---
+++ 08-JS-Front-End/04-Objects-and-Classes-Lab/02-City.js
@@ -0,0 +1,24 @@
+function cityInfo(input) {
+    let entries = Object.entries(input);
+
+    for (const [key, value] of entries) {
+        console.log(`${key} -> ${value}`);
+    }
+}
+
+cityInfo({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+}
+);
+cityInfo({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+}
+);
