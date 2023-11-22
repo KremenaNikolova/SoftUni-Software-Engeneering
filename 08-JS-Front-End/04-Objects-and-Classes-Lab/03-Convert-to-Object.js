fc8525e6ae4f0b3f36cb62592795ee0263bd4fc1
Kremena Nikolova <aradia1992@gmail.com>
2023-11-22 09:53:46 +0200

    initial commit

---
+++ 08-JS-Front-End/04-Objects-and-Classes-Lab/03-Convert-to-Object.js
@@ -0,0 +1,11 @@
+function convertObject(JSONinput) {
+    let object = JSON.parse(JSONinput);
+
+    let entries = Object.entries(object);
+    for (const [key, value] of entries) {
+        console.log(`${key}: ${value}`);
+    }
+}
+
+convertObject('{"name": "George", "age": 40, "town": "Sofia"}');
+convertObject('{"name": "Peter", "age": 35, "town": "Plovdiv"}');
