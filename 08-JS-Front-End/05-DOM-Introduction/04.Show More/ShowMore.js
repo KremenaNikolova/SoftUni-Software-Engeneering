940cb8187e7170e70590b8f8afe4f22b06349506
Kremena Nikolova <aradia1992@gmail.com>
2023-11-29 10:23:17 +0200

    initial commit

---
+++ 08-JS-Front-End/05-DOM-Introduction/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+  const hideElement = document.getElementById("more");
+  hideElement.style.display = "none";
+
+  const showElement = document.getElementById("text");
+  showElement.style.display = "inline";
+}
