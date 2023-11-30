b66e8d109ddefa3aaeeab99043711c429ac63543
Kremena Nikolova <aradia1992@gmail.com>
2023-11-30 19:50:30 +0200

    initial commit

---
+++ 08-JS-Front-End/05-DOM-Introduction-Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,10 @@
+function subtract() {
+  const firstNumber = Number(document.getElementById("firstNumber").value);
+  const secondNumber = Number(document.getElementById("secondNumber").value);
+
+  const result = firstNumber - secondNumber;
+
+  document.getElementById("result").textContent = result;
+
+  console.log(result);
+}
