96fcab42cf9dc01534a3e311554b69215ffb8f34
Kremena Nikolova <aradia1992@gmail.com>
2024-03-24 12:52:43 +0200

    add test if a user can add a task
    add instructions

---
+++ 10-DevOps/01-Automated-Testing-Lab/to-do-app/tests/todo.test.js
@@ -0,0 +1,20 @@
+//test files always at the end must be something.test.js
+//this tests are with Playwright so we must install it
+//npm install -D @playwright/test
+//if test doesnt work try ---> npx playwright install
+//to run test ---> npx playwright test
+
+const { test, expect } = require("@playwright/test"); //to import the test and expect functions from the Playwright Test library, we use this code
+
+test("user can add a task", async ({ page }) => {
+  //it has description "user can add a task" and an asynchronous function thet runs the test
+  await page.goto("http://localhost:5500/"); //then we navigate To-Do app. Note that the port may be different.
+
+  await page.fill("#task-input", "Test Task"); //we fill the task inout with the text 'Test Task'
+
+  await page.click("#add-task"); //and click the [Add Task] button, which should add the new task
+
+  const taskText = await page.textContent(".task"); //get the text content of the first element with class .task. After the [Add Task] button is clicked, this should be the task that was just added
+
+  expect(taskText).toContain("Test Task"); //it checks that the next of the added task includes 'Test Task'. If it does, the test passes. If it doesn't, the test fails;
+});
